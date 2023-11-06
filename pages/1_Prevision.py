782b7f0a7178b1f37ebd61369750d1f5af5f1b2c
Elmaystro <128915015+Elmaystro@users.noreply.github.com>
2023-11-06 14:17:01 +0100

    Update 1_Prevision.py

--- pages/1_Prevision.py
+++ pages/1_Prevision.py
@@ -1,14 +1,12 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 from pandas.tseries.offsets import DateOffset
-from pmdarima import auto_arima
 
 st.header("Téléchargez la liste des ventes")
 sales_file = st.file_uploader("Téléchargez un fichier Excel pour la liste des commandes", type=["xls", "xlsx"])
-s = st.slider("Les taille de la saison", 1, 24, 12)
+s = st.slider("Les taille de la saison", 1, 12, 12)
 
 # Add a button to show or hide the Liste des Ventes data frame
 show_data = st.checkbox("Afficher la Liste des Ventes")
@@ -22,34 +20,27 @@ if sales_file is not None:
         show_dataframe['Mois'] = show_dataframe['Mois'].dt.strftime('%B %Y')
         st.dataframe(show_dataframe)
 
-# Fit an ARIMA model using auto_arima
+# Fit a SARIMA model
 if calculate_button and sales_file is not None:
     sales = sales_df.copy()
-    data = sales_df.copy()
     sales['Mois'] = pd.to_datetime(sales['Mois'])
-    data['Mois'] = pd.to_datetime(sales['Mois'])
-    data.set_index('Mois', inplace=True)
-    data['Ventes'] = data['Ventes'].astype(float)  # Ensure the 'Ventes' column has a specific data type
-    try:
-        auto_model = auto_arima(data, seasonal=True, m=s)  # Adjust 'm' for the seasonality of your data
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
-        
-    # Get the best model's order (p, d, q) and seasonal order (P, D, Q, m)
-    order = auto_model.get_params()['order']
-    seasonal_order = auto_model.get_params()['seasonal_order']
-    st.write("seasonal_order", seasonal_order)
-    st.write("order", order)
-    # Adjust a SARIMA model to the data
+    sales.set_index('Mois', inplace=True)
+    sales['Ventes'] = sales['Ventes'].astype(float)  # Ensure the 'Ventes' column has a specific data type
+
+    order = (0, 1, 0)  # Order parameter (p, d, q)
+    seasonal_order = (1, 0, 1, s)  # Seasonal order parameter (P, D, Q, m)
+
+    # Fit a SARIMA model
     sarima_model = sm.tsa.SARIMAX(sales['Ventes'], order=order, seasonal_order=seasonal_order)
     sarima_results = sarima_model.fit()
+
     # Predict future sales using the SARIMA model
-    forecasted_sales = sarima_results.get_forecast(steps=12)
-    last_date = sales['Mois'].max()
+    forecasted_sales = sarima_results.get_forecast(steps=s)
+    last_date = sales.index.max()
     next_year = last_date.year + 1
 
     # Create a new DataFrame for the forecasted sales
-    forecasted_df = pd.DataFrame({'Mois': pd.date_range(start=sales['Mois'].max() + DateOffset(months=1), periods=12, freq='M'),
+    forecasted_df = pd.DataFrame({'Mois': pd.date_range(start=last_date + DateOffset(months=1), periods=s, freq='M'),
                                   'Ventes': forecasted_sales.predicted_mean.values})
 
     st.subheader("Ventes Prévues")
@@ -65,12 +56,10 @@ if calculate_button and sales_file is not None:
     
     # Calculate the figure size based on the number of data points
     num_data_points = len(sales_df) + len(forecasted_df)
-    fig_width =len(sales_df)/1.5  # Adjust the minimum width as needed
+    fig_width = len(sales_df) / 1.5  # Adjust the minimum width as needed
 
     # Create a chart with a dynamic figure size
-    #fig, ax = plt.subplots(figsize=(fig_width, 6))
-    # Create a chart with a larger figure size
-    fig, ax = plt.subplots(figsize=(fig_width,fig_width/4 ))
+    fig, ax = plt.subplots(figsize=(fig_width, fig_width / 4))
 
     ax.set_xlabel('Mois')
     ax.set_ylabel('Nombre de pièces commandées')
@@ -80,10 +69,14 @@ if calculate_button and sales_file is not None:
 
     ax.set_xticks(range(combined_len))
     ax.set_xticklabels(combined_df['Mois'], rotation=45, ha='right')
-
-    ax.bar(range(combined_len - 12), combined_df['Ventes'][:combined_len - 12], label='Demandes Réelles', alpha=0.7, color='blue')
-    ax.bar(range(combined_len - 12, combined_len), combined_df['Ventes'][combined_len - 12:], label='Demandes Prévues', alpha=0.7, color='green')
-    ax.plot(range(combined_len - 12, combined_len), combined_df['Ventes'][combined_len - 12:], linestyle='--', marker='o', color='red')
+    
+    ax.bar(range(combined_len - s), combined_df['Ventes'][:combined_len - s], label='Demandes Réelles', alpha=0.7, color='blue')
+    ax.bar(range(combined_len - s, combined_len), combined_df['Ventes'][combined_len - s:], label='Demandes Prévues', alpha=0.7, color='green')
+    ax.plot(range(combined_len - s, combined_len), combined_df['Ventes'][combined_len - s:], linestyle='--', marker='o', color='red')
     ax.legend()
+
+    # Draw vertical lines at each period based on the value of 's'
+    for i in range(0, len(combined_df), s):
+        ax.axvline(x=i, color='gray', linestyle='--')
 
     st.pyplot(fig)
